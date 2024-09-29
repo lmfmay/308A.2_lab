f160238676f60dc24c68b6c0f42805c50ba8c8e9
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-09-29 15:01:13 -0700

    Part 2 added

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 //Part 1: Humble Beginnings
 
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "Cat",
-        companion: {
-            name: "Frank",
-            type: "Flea",
-            inventory: ["small hat", "sunglasses"]
-        }
-    },
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
+//     companion: {
+//         name: "Leo",
+//         type: "Cat",
+//         companion: {
+//             name: "Frank",
+//             type: "Flea",
+//             inventory: ["small hat", "sunglasses"]
+//         }
+//     },
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//         }
+//     }
+
+//     adventurer.roll();
+
+// Part 2: Class Fantasy
+
+//Here is what the basic Character class looks like so far, including a constructor function that allows us to create new characters with whatever name we would like:
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+//Every character should also be able to make rolls. Add the roll method to the Character class.
     roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
         }
-    }
+}
 
-    adventurer.roll();
+//Now, re-create Robin using the Character class!
+const robin = new Character(`Robin`);
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-// Part 2: Class Fantasy
+console.log(robin.companion.companion);
